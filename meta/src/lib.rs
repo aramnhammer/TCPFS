54af042b7f77c14a203fb38f1a520f0a57a00382
aram yegiazaryan <aram@hey.com>
2024-09-04 15:07:29 -0700

    rethink how metadat is stored, use triggers to manage fs usage

--- meta/src/lib.rs
+++ meta/src/lib.rs
@@ -3,44 +3,180 @@ use std::{
     path::PathBuf,
 };
 
-use rusqlite::{Connection, Result, Transaction};
-
+use rusqlite::{params, Connection, Error, Result, Transaction};
 // FIXME: Build a common error thing
-type MetaError<T> = std::result::Result<T, MissUse>;
-
-#[derive(Debug, Clone)]
-struct MissUse;
-
-impl fmt::Display for MissUse {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Metadata creation missuse")
-    }
-}
 
 #[cfg(test)]
 mod tests {
+
+    use rusqlite::Error;
+
     use super::*;
+    extern crate tempdir;
+
+    fn init() -> Connection {
+        let con = get_connection().unwrap();
+        init_db(&con).unwrap();
+        return con;
+    }
 
     #[test]
-    fn test_connect() {}
+    fn test_triggers() {
+        let con = init();
+        con.execute(
+            "INSERT INTO objects (bucket_id, path, file_size) VALUES(?,?,?) ",
+            params!["test_id", "/some/file/path.txt", 1024],
+        )
+        .unwrap();
+        let mut stmt = con
+            .prepare("SELECT bucket_id, total_size FROM buckets WHERE bucket_id ='test_id'")
+            .unwrap();
+        let result: (String, i64) = stmt
+            .query_row([], |row| {
+                Ok((
+                    row.get(0)?, // bucket_id
+                    row.get(1)?, // total_size
+                ))
+            })
+            .unwrap();
+
+        assert_eq!(result.0, "test_id");
+        assert_eq!(result.1, 1024);
+
+        con.execute(
+            "INSERT INTO objects (bucket_id, path, file_size) VALUES(?,?,?) ",
+            params!["test_id", "/some/file/path2.txt", 1024],
+        )
+        .unwrap();
+
+        let mut stmt = con
+            .prepare("SELECT bucket_id, total_size FROM buckets WHERE bucket_id ='test_id'")
+            .unwrap();
+        let result: (String, i64) = stmt
+            .query_row([], |row| {
+                Ok((
+                    row.get(0)?, // bucket_id
+                    row.get(1)?, // total_size
+                ))
+            })
+            .unwrap();
+
+        assert_eq!(result.0, "test_id");
+        assert_eq!(result.1, 2048);
+    }
+
+    #[test]
+    fn test_insert_and_delete() {
+        let mut con = init();
+        let tx = start_transaction(&mut con);
+        insert_metadata(&tx, "testid", "/path", "1024").unwrap();
+        tx.commit().unwrap();
+        let result: (String, String) = con
+            .prepare("SELECT bucket_id, path FROM objects WHERE bucket_id='testid'")
+            .unwrap()
+            .query_row([], |row| Ok((row.get(0)?, row.get(1)?)))
+            .unwrap();
+        assert_eq!(
+            (result.0, result.1),
+            ("testid".to_string(), "/path".to_string())
+        );
+        let bucket_total_size: i32 = con
+            .prepare("SELECT total_size FROM buckets WHERE bucket_id = 'testid'")
+            .unwrap()
+            .query_row([], |row| row.get(0))
+            .unwrap();
+
+        assert_eq!(bucket_total_size, 1024);
+
+        let tx = start_transaction(&mut con);
+        delete_metadata(&tx, "testid", "/path").unwrap();
+        tx.commit().unwrap();
+
+        let bucket_total_size: i32 = con
+            .prepare("SELECT total_size FROM buckets WHERE bucket_id = 'testid'")
+            .unwrap()
+            .query_row([], |row| row.get(0))
+            .unwrap();
+
+        assert_eq!(bucket_total_size, 0);
+    }
 }
 
 pub fn get_connection() -> Result<Connection> {
     return Connection::open_in_memory();
 }
 
-pub fn init_db(conn: &Connection, bucket_id: &String) -> Result<()> {
-    // Construct the SQL statement with the table name
-    let sql = format!(
-        "CREATE TABLE IF NOT EXISTS obj_{} (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            bucket_id TEXT NOT NULL UNIQUE,
-            path TEXT NOT NULL,
-            size INTEGER
-        )",
-        bucket_id
+pub fn init_db(conn: &Connection) -> Result<()> {
+    // create object table
+    conn.execute(
+        " -- reference table for buckets (root dirs, and their metadata)
+    CREATE TABLE buckets (
+    bucket_id TEXT PRIMARY KEY,   -- UUID of the top-level folder
+    total_size INTEGER DEFAULT 0
     );
-    let _ = conn.execute(&sql, ());
+    ",
+        params![],
+    )
+    .unwrap();
+
+    conn.execute(
+        " -- reference table for objects on disk
+    CREATE TABLE objects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    bucket_id TEXT NOT NULL,
+    path TEXT,
+    file_size INTEGER,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(bucket_id, path),
+    FOREIGN KEY (bucket_id) REFERENCES buckets(bucket_id)
+    ON DELETE CASCADE);
+    ",
+        params![],
+    )
+    .unwrap();
+
+    conn.execute("-- Trigger to update the total size when a file reference is inserted, handle case if bucket does not exists (no other files there)
+    CREATE TRIGGER update_total_size_after_insert
+    AFTER INSERT ON objects 
+    FOR EACH ROW
+    BEGIN
+        -- Insert bucket_id into objects if it does not exist, then update the total size
+        INSERT INTO buckets (bucket_id, total_size)
+        VALUES (NEW.bucket_id, NEW.file_size)
+        ON CONFLICT(bucket_id)
+        DO UPDATE SET total_size = total_size + NEW.file_size;
+    END; 
+    ", params![]).unwrap();
+
+    conn.execute(
+        "-- Trigger to update the total size when a file reference is updated
+    CREATE TRIGGER update_total_size_after_update
+    AFTER UPDATE OF file_size ON objects 
+    FOR EACH ROW
+    BEGIN
+        UPDATE buckets 
+        SET total_size = total_size - OLD.file_size + NEW.file_size
+        WHERE bucket_id = NEW.bucket_id;
+    END;
+    ",
+        params![],
+    )
+    .unwrap();
+
+    conn.execute(
+        "-- Trigger to update the total size when a file reference is deleted
+    CREATE TRIGGER update_total_size_after_delete
+    AFTER DELETE ON objects 
+    FOR EACH ROW
+    BEGIN
+        UPDATE buckets 
+        SET total_size = total_size - OLD.file_size
+        WHERE bucket_id = OLD.bucket_id;
+    END;
+    ",
+        params![],
+    )
+    .unwrap();
     Ok(())
 }
 
@@ -48,31 +184,25 @@ pub fn start_transaction(conn: &mut Connection) -> Transaction {
     return conn.transaction().unwrap();
 }
 
-// Function to insert new metadata within a transaction
-pub fn insert_metadata(tx: &Transaction, bucket_id: &str, path: &str, size: u32) -> Result<()> {
-    let tbl = format!("INSERT INTO obj_{} ", bucket_id);
-    tx.execute(
-        &(tbl + "(bucket_id, path, size, last_modified) VALUES (?, ?, ?, ?)"),
-        (bucket_id, path, size),
-    )
-    .unwrap();
-    Ok(())
+pub fn delete_metadata(tx: &Transaction, bucket_id: &str, path: &str) -> Result<usize, Error> {
+    Ok(tx
+        .execute(
+            "DELETE FROM objects WHERE bucket_id = ? AND path = ?",
+            &[bucket_id, path],
+        )
+        .unwrap())
 }
 
-pub fn get_metadata(conn: &Connection, path: &str) -> Result<Option<(i64, String)>> {
-    let stmt = conn.prepare("SELECT size, last_modified FROM objects WHERE bucket_id = ?");
-    let mut binding = stmt.unwrap();
-    let mut rows = binding.query(&[path]).unwrap();
-
-    if let Some(row) = rows.next().unwrap() {
-        let size: i64 = row.get(0)?;
-        let last_modified: String = row.get(1)?;
-        Ok(Some((size, last_modified)))
-    } else {
-        Ok(None)
-    }
-}
-
-pub fn delete_metadata(tx: &Transaction, bucket_id: &str) -> Result<usize> {
-    tx.execute("DELETE FROM objects WHERE bucket_id = ?", &[bucket_id])
+pub fn insert_metadata(
+    tx: &Transaction,
+    bucket_id: &str,
+    path: &str,
+    size: &str,
+) -> Result<usize, Error> {
+    Ok(tx
+        .execute(
+            "INSERT INTO objects (bucket_id, path, file_size) VALUES(?,?,?)",
+            &[bucket_id, path, size],
+        )
+        .unwrap())
 }
